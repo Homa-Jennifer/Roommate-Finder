8fa92894bd3f84d8c3e38f4279cf56bdcf03e796
thatzerroguy <ndukaugochukwu9@gmail.com>
2024-12-19 10:50:42 +0100

    Add Button component and update Tailwind config
    Created a reusable `Button` component with primary and secondary styles. Updated the Tailwind configuration to extend the theme with custom colors and include the `./components` directory in the content paths. Refactored `app/index.tsx` to use the new `Button` component.

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,13 @@
 import { Text, View } from "react-native";
+import Button from "@/components/Button";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
+        className="px-4 justify-center items-center"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-        <View className="bg-amber-200 w-4 h-4 rounded-3xl"></View>
-        <View className="w-10 h-10 rounded-3xl bg-amber-200"></View>
+        <Button title={"Continue"} type={"primary"} />
     </View>
   );
 }
